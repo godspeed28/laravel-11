5658517ef2b6850e315a64d276576329bb004969
godspeed28 <albertog4taz28@gmail.com>
2025-07-23 17:40:26 +0700

    add BlogController

--- controller-12/resources/views/blog.blade.php
+++ controller-12/resources/views/blog.blade.php
@@ -13,7 +13,6 @@
         {{ $posts->links() }}
     </div>
 
-
     <div
         class="mb-10 mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         @foreach ($posts as $post)
